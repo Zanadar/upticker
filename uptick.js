2880feecc18d965bcd8028120d8b029125253922
Zander Mackie <zmackie@gmail.com>
2016-02-10 17:54:09 -0500

    Initial commit

---
+++ uptick.js
@@ -0,0 +1,15 @@
+var through = require('through2')
+var split = require('split')
+
+var count = 0
+
+process.stdin
+  .pipe(split())
+  .pipe(through(function (line, _, done) {
+    var bracket_digit = /\[\d+/g
+    var replacer = '[' + count
+    line.replace(bracket_digit, replacer)
+    count++
+    done()
+  }))
+  .pipe(process.stdout)
